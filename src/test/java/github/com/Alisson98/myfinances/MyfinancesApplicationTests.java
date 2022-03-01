b4e682c6a1530c6be0215b8a591028cb2caf93b5
Alisson98 <alissonfernandes98@outlook.com>
2022-03-01 11:36:04 -0300

    first commit

---
+++ src/test/java/github/com/Alisson98/myfinances/MyfinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.Alisson98.myfinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
