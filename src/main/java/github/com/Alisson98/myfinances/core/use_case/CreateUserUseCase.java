f5aaef3257564d4ad73db3dc6ddb7c2af5722be0
Alisson98 <alissonfernandes98@outlook.com>
2022-03-02 15:10:26 -0300

    Implemented CreateUserUseCase

---
+++ src/main/java/github/com/Alisson98/myfinances/core/use_case/CreateUserUseCase.java
@@ -0,0 +1,28 @@
+package github.com.Alisson98.myfinances.core.use_case;
+
+import github.com.Alisson98.myfinances.core.entities.User;
+import github.com.Alisson98.myfinances.core.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateUserUseCase {
+    private static final Logger logger = LoggerFactory.getLogger(CreateUserUseCase.class);
+
+    private final UserRepository userRepository;
+    private final EmailUserValidationUseCase emailUserValidationUseCase;
+
+    public CreateUserUseCase(UserRepository userRepository, EmailUserValidationUseCase emailUserValidationUseCase) {
+        this.userRepository = userRepository;
+        this.emailUserValidationUseCase = emailUserValidationUseCase;
+    }
+
+    public User execute(User user){
+        logger.info("Saving User with name: {}", user.getName());
+        emailUserValidationUseCase.execute(user.getEmail());
+        userRepository.save(user);
+        logger.info("User with name {} saved successfully", user.getName());
+        return user;
+    }
+}
